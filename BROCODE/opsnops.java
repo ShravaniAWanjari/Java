c2fc493ec6b09b3085bedffbdea8b6f3ce5fe0b5
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-07-26 11:31:54 +0530

    operators and operands

---
+++ BROCODE/opsnops.java
@@ -0,0 +1,13 @@
+package BROCODE;
+
+public class opsnops {
+    public static void main(String[] args) {
+        double friends = 10;
+        friends += 1;
+
+        friends = (double) friends / 3;
+
+        System.out.println(friends);
+    }
+
+}
