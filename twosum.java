edc4533bc58e21e6d34cf9187925ae7ebb6056c0
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-07-29 09:45:20 +0530

    fixed two sum

--- twosum.java
+++ twosum.java
@@ -7,10 +7,11 @@ public class twosum {
         for (int i : list1) {
             List<Integer> result = new ArrayList<>();
             if (list1.contains(target - i)) {
-                int complementIndex = list1.indexOf(target - i);
+                int currentIndex = list1.indexOf(i);
+                int complementIndex = list1.lastIndexOf(target - i);
                 if (complementIndex != list1.indexOf(i)) {
-                    result.add(list1.indexOf(i));
-                    result.add(list1.indexOf(target - i));
+                    result.add(currentIndex);
+                    result.add(complementIndex);
                     return result;
                 }
             }
@@ -21,8 +22,8 @@ public class twosum {
 
     public static void main(String[] args) {
         twosum solver = new twosum();
-        List<Integer> list1 = new ArrayList<>(List.of(1, 4, 5, 7, 8, 9));
-        int target = 9;
+        List<Integer> list1 = new ArrayList<>(List.of(3, 3, 4, 5, 7, 8, 9));
+        int target = 6;
         List<Integer> result = solver.twoSum(list1, target);
         System.out.println(result);
     }
