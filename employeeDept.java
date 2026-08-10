e69f49eee891e91f29c0e7f8f882d091fed41f59
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-10 15:15:18 +0530

    employee dept sort

---
+++ employeeDept.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+class Employee {
+    String name;
+    String Dept;
+
+    Employee(String name, String Dept) {
+        this.name = name;
+        this.Dept = Dept;
+    }
+}
+
+public class employeeDept {
+    public Map<String, List<String>> sortDept(List<Employee> employees) {
+        Map<String, List<String>> userMap = new HashMap<>();
+        for (Employee i : employees) {
+            userMap.put(i.Dept, userMap.getOrDefault(i.Dept, new ArrayList<>()));
+            userMap.get(i.Dept).add(i.name);
+        }
+        return userMap;
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee("Amit", "Engineering"),
+                new Employee("Ravi", "Sales"),
+                new Employee("Neha", "Engineering"),
+                new Employee("Priya", "HR"),
+                new Employee("Rahul", "Sales")));
+
+        employeeDept solution = new employeeDept();
+        System.out.println(solution.sortDept(employees));
+    }
+}
