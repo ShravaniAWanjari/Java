0d0e759a1ceca5abe8a1ced8d178e5d06bd9a9ab
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-10 14:25:22 +0530

    mostFreq user

---
+++ Mappractice/mostFrequser.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class mostFrequser {
+    public String findFrequser(String[] users) {
+        Map<String, Integer> userMap = new HashMap<>();
+        int maxUserfreq = 0;
+        String maxUser = "";
+
+        for (String user : users) {
+            userMap.put(user, userMap.getOrDefault(user, 0) + 1);
+            if (userMap.get(user) > maxUserfreq) {
+                maxUserfreq = userMap.get(user);
+                maxUser = user;
+            }
+        }
+        return maxUser;
+    }
+
+    public static void main(String[] args) {
+        mostFrequser solver = new mostFrequser();
+        String[] users = { "A101", "A102", "A101", "A103", "A102", "A101", "A102", "A102" };
+        System.out.println(solver.findFrequser(users));
+    }
+}
