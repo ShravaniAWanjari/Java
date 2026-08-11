d4bb934dc703c964a24e6184a00e040a2079f936
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-11 22:49:36 +0530

    user service

---
+++ UserService.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class UserService {
+
+    private List<String> users;
+
+    public UserService(List<String> users) {
+        this.users = users;
+    }
+
+    public int getUserCount() {
+        if (users.isEmpty()) {
+            return 0;
+        } else {
+            return users.size();
+        }
+    }
+
+    public void printUsers() {
+        for (String user : users) {
+            System.out.println(user);
+        }
+    }
+}
