27103e766e23620a87e9de8789a3d8634c8a44b9
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-11 23:05:39 +0530

    score service

---
+++ ScoreService.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+import java.util.*;
+
+class ScoreService {
+
+    private Map<String, Integer> scores;
+
+    public ScoreService(Map<String, Integer> scores) {
+        this.scores = scores;
+    }
+
+    public List<String> getUsersAboveScore(int threshold) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : scores.entrySet()) {
+            if (entry.getValue() > threshold) {
+                result.add(entry.getKey());
+            }
+        }
+        return result;
+    }
+
+    public Integer getScore(String username) {
+        return scores.get(username);
+    }
+}
