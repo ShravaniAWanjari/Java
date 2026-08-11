80a79cd215748436598042e697d52c19cbfb7af5
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-11 14:22:03 +0530

    group employees

---
+++ employeeDept.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Employee {
+    String name;
+    String department;
+
+    Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
+public class employeeDept {
+    static Map<String, List<String>> groupEmployees(List<Employee> employees) {
+        Map<String, List<String>> employeeMap = new HashMap<>();
+        for (Employee i : employees) {
+            employeeMap.computeIfAbsent(i.department, k -> new ArrayList<>()).add(i.name);
+        }
+        return employeeMap;
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employees = List.of(
+                new Employee("Arial", "Engineering"),
+                new Employee("Priya", "Research"),
+                new Employee("Riya", "Medical"),
+                new Employee("Rahul", "Sales"),
+                new Employee("Neha", "Engineering"));
+
+        System.out.println(groupEmployees(employees));
+    }
+}
