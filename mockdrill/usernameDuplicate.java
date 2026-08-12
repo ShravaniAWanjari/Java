3cb2be008f8abddd4e3eeba3d4d16d1a8eec28ea
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-12 07:23:03 +0530

    username duplicate using set

---
+++ mockdrill/usernameDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class usernameDuplicate {
+
+    private List<String> users;
+
+    public usernameDuplicate(List<String> users) {
+        this.users = users;
+    }
+
+    public List<String> getUniqueUsers() {
+        Set<String> setUsers = new HashSet<>(users);
+
+        return new ArrayList<>(setUsers);
+    }
+
+    public int getUserCount() {
+        return users.size();
+    }
+
+    public static void main(String[] args) {
+        List<String> users = new ArrayList<>(List.of(
+                "Amit", "Ravi", "Amit", "Neha", "Ravi", "Amit"));
+
+        usernameDuplicate solver = new usernameDuplicate(users);
+        System.out.println(solver.getUniqueUsers());
+    }
+}
