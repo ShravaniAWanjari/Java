7dd0b0161a8197edb670b2bd58108b454a460627
Shravani Ankur Wanjari <shravani22wanjari@gmail.com>
2026-08-12 08:09:47 +0530

    login attempt

---
+++ mockdrill/LoginService.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class LoginService {
+
+    private Map<String, Integer> loginAttempts;
+
+    public LoginService(Map<String, Integer> loginAttempts) {
+        this.loginAttempts = loginAttempts;
+    }
+
+    public List<String> getUsersNeedingReview() {
+        List<String> loginList = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : loginAttempts.entrySet()) {
+            if (entry.getValue() == null) {
+                continue;
+            } else if (entry.getValue() >= 5) {
+                loginList.add(entry.getKey());
+            }
+        }
+        return loginList;
+    }
+}
